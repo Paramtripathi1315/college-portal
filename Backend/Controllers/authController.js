ade2f0d89719690d88415374dfc6ba0ffb4a4103
Paramtripathi1315 <paramt1315@gmail.com>
2025-07-11 11:07:06 +0530

    register backend

--- Backend/Controllers/authController.js
+++ Backend/Controllers/authController.js
@@ -9,7 +9,7 @@ const register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
 
-    if (!name || !email || !password || !role) {
+    if (!name || !email || !password) {
       return res.status(400).json({ success: false, message: 'All fields are required.' });
     }
 
