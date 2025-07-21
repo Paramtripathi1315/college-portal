836b95eca119524f5ceabd2e61c7a6f906ccd99b
Paramtripathi1315 <paramt1315@gmail.com>
2025-07-21 10:57:55 +0530

    changes in controller

--- Backend/Routes/adminRoutes.js
+++ Backend/Routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verifyToken, requireAdmin } = require('../middleware/authMiddleware');
+const { verifyToken, requireAdmin } = require('../Middleware/authMiddleware');
 
 // @desc    Admin Dashboard
 // @route   GET /api/admin/dashboard
